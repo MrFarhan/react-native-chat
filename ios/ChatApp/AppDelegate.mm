df01bb53137c40f55bfbd5a4ba83cc6e2217bb4c
MrFarhan <muhammadfarhan012345@gmail.com>
2024-05-24 01:06:20 +0500

    Installed firebase

--- ios/ChatApp/AppDelegate.mm
+++ ios/ChatApp/AppDelegate.mm
@@ -3,6 +3,9 @@
 #import <React/RCTBundleURLProvider.h>
 
 #import "RNSplashScreen.h"
+#import <Firebase.h>
+
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -11,6 +14,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  [FIRApp configure];
   
   BOOL ret = [super application:application didFinishLaunchingWithOptions:launchOptions];
   if (ret == YES) {
